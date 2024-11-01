1cb2ab9b6f7f2df39e2f8ab360975127bce5c625
mdnuruzzamannirob <nuruzzamanmd2002@gmail.com>
2024-11-01 16:40:29 +0600

    added eslint prettier and lint staged setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,34 +1,34 @@
-import type { Config } from "tailwindcss";
-import animate from "tailwindcss-animate";
+import type { Config } from 'tailwindcss';
+import animate from 'tailwindcss-animate';
 
 const config: Config = {
-  darkMode: ["class"],
+  darkMode: ['class'],
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       screens: {
-        xs: "360px", // extra small
-        sm: "480px", // small
-        md: "640px", // medium
-        lg: "768px", // large
-        xl: "992px", // extra large
-        "2xl": "1280px", // Small laptops
-        "3xl": "1440px", // Large laptops
-        "4xl": "1920px", // Medium desktops
-        "5xl": "2560px", // Large desktops
+        xs: '360px', // extra small
+        sm: '480px', // small
+        md: '640px', // medium
+        lg: '768px', // large
+        xl: '992px', // extra large
+        '2xl': '1280px', // Small laptops
+        '3xl': '1440px', // Large laptops
+        '4xl': '1920px', // Medium desktops
+        '5xl': '2560px', // Large desktops
       },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
       },
       borderRadius: {
-        lg: "var(--radius)",
-        md: "calc(var(--radius) - 2px)",
-        sm: "calc(var(--radius) - 4px)",
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)',
       },
     },
   },
