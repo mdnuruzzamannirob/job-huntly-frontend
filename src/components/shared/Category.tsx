3756540d83a9ec020de6b06120c0b340a3f4786e
mdnuruzzamannirob <nuruzzamanmd2002@gmail.com>
2024-11-08 02:16:44 +0600

    completed category

--- src/components/shared/Category.tsx
+++ src/components/shared/Category.tsx
@@ -4,10 +4,17 @@ import DynamicIcon from './DynamicIcon';
 const Category = ({ category }: { category: any }) => {
   return (
     <div className="group rounded border p-4 duration-200 ease-linear hover:bg-primary/5">
-      <div className="size-12 rounded-full p-[2px] group-hover:bg-primary">
-        <DynamicIcon iconFamily={category.iconFamily} icon={category.icon} />
+      <div className="size-12 rounded-full text-4xl duration-200 ease-linear group-hover:text-primary">
+        <p>
+          <DynamicIcon
+            iconFamily={category.iconFamily.trim()}
+            icon={category.icon.trim()}
+          />
+        </p>
       </div>
-      <h3 className="mb-3 mt-6 text-xl font-bold">{category.name}</h3>
+      <h3 className="mb-3 mt-6 text-xl font-bold group-hover:text-primary">
+        {category.name}
+      </h3>
       <button className="flex flex-nowrap items-center gap-3 font-medium text-neutrals-600">
         {category.totalJobs} jobs available <HiOutlineArrowNarrowRight />
       </button>
