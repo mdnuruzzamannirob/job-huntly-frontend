661399c0746a5935f660e7c6fe35fe0720d7fc4e
mdnuruzzamannirob <nuruzzamanmd2002@gmail.com>
2024-11-08 12:48:47 +0600

    fixed responsiveness in category section

--- src/components/shared/home/CategorySection.tsx
+++ src/components/shared/home/CategorySection.tsx
@@ -2,26 +2,34 @@ import Container from '@/components/custom-ui/Container';
 import Button from '@/components/custom-ui/Button';
 import { HiOutlineArrowNarrowRight } from 'react-icons/hi';
 import { categories } from '@/constants/categories';
-import Category from '../Category';
+import CategoryCard from '../CategoryCard';
 
 const CategorySection = () => {
   return (
     <Container className="py-10 md:py-16 xl:py-20">
       <div className="flex items-center justify-between">
-        <h1 className="text-3xl font-extrabold">
+        <h1 className="text-xl font-extrabold md:text-2xl xl:text-3xl">
           Explore by <span className="text-primary">category</span>
         </h1>
         <Button
           variant="ghost"
-          className="flex items-center justify-center gap-2"
+          className="hidden items-center justify-center gap-2 md:flex"
         >
           Show all category <HiOutlineArrowNarrowRight />
         </Button>
       </div>
-      <div className="mt-10 grid grid-cols-5 gap-5">
+      <div className="mt-10 grid grid-cols-1 gap-5 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
         {categories.slice(0, 15).map((category: any) => (
-          <Category key={category._id} category={category} />
+          <CategoryCard key={category._id} category={category} />
         ))}
+      </div>
+      <div className="flex items-center justify-end md:hidden">
+        <Button
+          variant="ghost"
+          className="mt-5 flex items-center justify-center gap-2"
+        >
+          Show all category <HiOutlineArrowNarrowRight />
+        </Button>
       </div>
     </Container>
   );
